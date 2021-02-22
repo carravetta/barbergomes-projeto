4745a7cf0a0506b136f212090b7f29a6c008335d
Lucas Carravetta <lucas_carravetta@hotmail.com>
2021-02-22 15:58:23 -0300

    atualizações

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$("#modal-horarios").on('shown.bs.modal', function () {
+    $('.horarios-btn').trigger('focus')
+  })
+
+$(".navbar-toggler-icon").click(function (){
+  $(".banner").toggle()
+})
+  
